76eadb63b4f937a24d1a2984fc38c1a340a7ae24
GalileoCap <galileocapp@gmail.com>
2022-04-16 02:35:27 -0300

    OK: Optimizations

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-g++ -o tp1 -D _ENTREGA_ src/main.cc
-g++ -o tp1.debug src/main.cc
-g++ -o tp1.nband -D _NBAND_ src/main.cc
-g++ -o tp1.float -D _FLOAT_ src/main.cc
+g++ -O3 -o tp1 -D _ENTREGA_ src/main.cc
+g++ -O3 -o tp1.debug src/main.cc
+g++ -O3 -o tp1.nband -D _NBAND_ src/main.cc
+g++ -O3 -o tp1.float -D _FLOAT_ src/main.cc
