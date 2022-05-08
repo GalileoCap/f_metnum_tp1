a35019eb72da77d73c75e115083ce13448d26625
GalileoCap <galileocapp@gmail.com>
2022-05-08 12:26:17 -0300

    FIX: Casos uniformes

--- analysis/analyze_uniform.py
+++ analysis/analyze_uniform.py
@@ -6,10 +6,11 @@ import plot
 def example_uniform(data, ti_step, te_step, fpath):
 	[_, _, mp1, n, _, _], _ = data
 
+	ti, te = 1500, 250
 	T = [
-		[1500 + i * ti_step for i in range(n)] + [250 + i * te_step for i in range(n)], #A: Caso 0
-		[1500 + i * ti_step for i in range(n)] + [250 for i in range(n)], #A: Caso 1
-		[1500 + i * ti_step for i in range(n)] + [250 - (n - i) * te_step for i in range(n)], #A: Caso 2
+		[ti + i * ti_step for i in range(n // 2)] + [ti + (n // 2 - i) * ti_step for i in range(n // 2)]  + [te for _ in range(n)], #A: Caso 1
+		[ti + i * ti_step for i in range(n // 2)] + [ti + (n // 2 - i) * ti_step for i in range(n // 2)]  + [te + i * te_step for i in range(n // 2)] + [te + (n // 2 - i) * te_step for i in range(n // 2)], #A: Caso 2
+		[ti + i * ti_step for i in range(n // 2)] + [ti + (n // 2 - i) * ti_step for i in range(n // 2)]  + [te + (n // 2 - i) * te_step for i in range(n // 2)] + [te + i * te_step for i in range(n // 2)], #A: Caso 3
 	]
 
 	with open(in_fpath(fpath), 'w') as fout:
@@ -18,7 +19,7 @@ def example_uniform(data, ti_step, te_step, fpath):
 			fout.write(f'{" ".join([str(t) for t in inst])}\n')
 
 def uniform(ti_step, te_step, fpath, replace = True):
-	data = [1, 3, 25, 25, 500, 3], None
+	data = [1, 3, 30, 10, 500, 3], None
 
 	if replace:
 		example_uniform(data, ti_step, te_step, fpath)
